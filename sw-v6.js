fc7d2f3343ecf4cfda441e5cf3afc28a2e5642e0
seungwoo-lee1233 <seungwoo4054@gmail.com>
2025-08-08 23:13:55 +0900

    Delete sw-v6.js

--- sw-v6.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('shiftcal-v6').then(cache => cache.addAll([
-    './','./index.html','./styles.css','./manifest.webmanifest','./holidays.json','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
