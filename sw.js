24737560b59a902cc6fac63b0d9c09e4d651a488
seungwoo-lee1233 <seungwoo4054@gmail.com>
2025-08-08 21:14:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,9 @@
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('shiftcal-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './styles.css',
-      './manifest.webmanifest',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
-  );
+  event.waitUntil(caches.open('shiftcal-v3').then(cache => cache.addAll([
+    './','./index.html','./styles.css','./manifest.webmanifest','./holidays.json','./icon-192.png','./icon-512.png'
+  ])));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
